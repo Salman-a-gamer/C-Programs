47b11035a154d4482d23f4d00d0e3cc7202fc459
Salman Imran Syed <salmanimransyed@gmail.com>
2024-10-07 11:58:00 +0530

    Few changes

--- Practise Questions/1.c
+++ Practise Questions/1.c
@@ -1,1 +1,7 @@
+/* C program to print a simple message.*/
 
+#include <stdio.h>
+void main(){
+	printf("Welcome to C programming");
+	return;
+}
--- Practise Questions/2.c
+++ Practise Questions/2.c
@@ -1,5 +1,5 @@
+/*C Program demonstrating to display all the primitive data type variables.*/
 #include <stdio.h>
-
 void main(){
 	int dec=10;
 	char ch = 'p';

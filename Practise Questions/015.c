60264105f61b93ffcd295c48e21a14c0754e5292
Salman Imran Syed <salmanimransyed@gmail.com>
2024-10-09 17:50:04 +0530

    Modified 015.c

--- Practise Questions/015.c
+++ Practise Questions/015.c
@@ -1,10 +1,13 @@
 /*C Program to swap given two numbers.*/
 #include <stdio.h>
 int main(){
-	int x,y;
+	int x,y,temp;
 	printf("Enter the values of x and y: ");
 	scanf("%d%d", &x, &y);
 	printf("Before swapping x = %d, y = %d\n", x,y);
-	printf("After swapping x = %d, b = %d\n", y,x);
+	temp = y;
+	y = x;
+	x = temp;
+	printf("After swapping x = %d, b = %d\n", x,y);
     return 0;
 }

d4020b56a98150427b28823b48babd878aa6ff69
Salman Imran Syed <salmanimransyed@gmail.com>
2024-10-09 17:48:06 +0530

    Updated 027.c

--- Practise Questions/027.c
+++ Practise Questions/027.c
@@ -4,10 +4,6 @@ int main(){
 	int a;
 	printf("Enter an integer: ");
 	scanf("%d", &a);
-		if(a%2==0){
-		printf("The given number is even number\n");
-		} else{
-			printf("The given number is odd number\n");
-		}
-    return 0;
+	(a%2==0) ? printf("The given number is even number\n"): printf("The given number is odd number\n");
+	return 0;	
 }

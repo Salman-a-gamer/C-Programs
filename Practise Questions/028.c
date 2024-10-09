ab89a578fd3b6ce93c7697ad192a1ab367e2ec23
Salman Imran Syed <salmanimransyed@gmail.com>
2024-10-09 18:59:15 +0530

    Changed 028.c

--- Practise Questions/028.c
+++ Practise Questions/028.c
@@ -1,15 +1,10 @@
 /*program to find the biggest of three numbers using a conditional operator.*/
 #include <stdio.h>
 int main(){
-	int a,b,c;
+	int a,b,c,x;
 	printf("Enter three numbers: ");
 	scanf("%d%d%d", &a,&b,&c);
-	if(a>b && a>c){
-		printf("The biggest number is: %d\n", a);
-	} else if(b>a && b>c){
-		printf("The biggest number is: %d\n", b);
-	} else {
-		printf("The biggest number is: %d\n", c);
-	}
-    return 0;
+ 	x = (a>b && a>c) ? a : ( (b>a && b>c) ? b : c );
+	printf("The biggest number is: %d\n", x);
+	return 0;
 }
